b7b7e7211f590bd4cb1c0df43f7176b33fe7d3dc
Enzo <d900124@gmail.com>
2024-06-27 18:14:15 +0800

    first commit

---
+++ src/main/java/com/kajarda/MiddleprojectApplication.java
@@ -0,0 +1,13 @@
+package com.kajarda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddleprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiddleprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enzo/demo/MiddleprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enzo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiddleprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
