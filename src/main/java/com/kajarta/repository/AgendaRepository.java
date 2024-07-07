d6dde15e46f930673808bfacf716d396de63b1a2
stanley1210 <stbs1210@gmail.com>
2024-07-07 16:13:14 +0800

    7/7

--- src/main/java/com/kajarta/repository/AgendaRepository.java
+++ src/main/java/com/kajarta/repository/AgendaRepository.java
@@ -3,6 +3,7 @@ package com.kajarta.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.kajarta.demo.model.Agenda;
 
+
 public interface AgendaRepository extends JpaRepository<Agenda, Integer> {
 
 }
