ef05f13f67f6a66727d2f5ba8f3f43e3cf943c49
Enzo <d900124@gmail.com>
2024-07-23 17:30:30 +0800

    spring-F搬入spring-B

---
+++ src/main/java/com/kajarta/repository/CarRepository.java
@@ -0,0 +1,16 @@
+package com.kajarta.repository;
+
+import com.kajarta.demo.model.Car;
+import com.kajarta.demo.model.Preference;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, Integer>, JpaSpecificationExecutor<Car> {
+
+}
