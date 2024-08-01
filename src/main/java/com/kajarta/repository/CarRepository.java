3e5b083f016b0f9ada7ad1962e7cbd1119632cbe
Vic <g27942110n@gmail.com>
2024-08-01 09:31:12 +0800

    搜尋customerId

--- src/main/java/com/kajarta/repository/CarRepository.java
+++ src/main/java/com/kajarta/repository/CarRepository.java
@@ -9,9 +9,13 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CarRepository extends JpaRepository<Car, Integer>, JpaSpecificationExecutor<Car> {
-List<Car> findByCreateTimeAfter(LocalDateTime since);
+    List<Car> findByCreateTimeAfter(LocalDateTime since);
+
+    @Query(value = "SELECT * FROM Car WHERE customer_id= :Id", nativeQuery = true)
+    List<Car> findByCustomerId(Integer Id); // 搜尋會員心儀清單
 }
