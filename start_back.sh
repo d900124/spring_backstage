aa34ae9a21a3b6db825cda7689d615e16bb2eaaa
sasha <yellow19950217@gmail.com>
2024-08-04 00:39:49 +0800

    update shell

--- start_back.sh
+++ start_back.sh
@@ -1,1 +1,5 @@
+#linux
 nohup java -Xms512m -Xmx1024m -jar ./target/spring_backstage-0.0.1-SNAPSHOT.jar --server.port=8080 > output.log 2>&1 &
+
+#windows
+Start-Process cmd.exe -ArgumentList "/c java -Xms512m -Xmx1024m -jar ./target/spring_backstage-0.0.1-SNAPSHOT.jar --server.port=8080 > output.log 2>&1" -NoNewWindow
